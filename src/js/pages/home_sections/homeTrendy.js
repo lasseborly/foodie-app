78f840a54e51180afacffe7d222520a67097fe6c
matis-dk <mathiasp50@gmail.com>
2019-09-10 22:36:03 +0200

    Progress for fluid transition

--- src/js/pages/home_sections/homeTrendy.js
+++ src/js/pages/home_sections/homeTrendy.js
@@ -1,52 +1,28 @@
-import React, {useState, useRef} from "react";
-import styled from '@emotion/styled'
-import { Div, P } from '../../layouts/layout'
-import { headerCardPrimary, headerCardSecondary, headerCardPrimaryTrendy, headerCardTiny, headerSliderNavigation } from '../../components/typography'
-import { colors, shadows } from '../../../style/theme'
-import { IconKcal, IconHeart, IconPeople } from '../../../img/icons/Icons'
-import { useInView } from 'react-intersection-observer'
-import { useSpring, animated, interpolate} from 'react-spring'
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { Div, P } from "../../layouts/layout";
+import { headerCardSecondary, headerCardPrimaryTrendy, headerSliderNavigation } from "../../components/typography";
+import { colors, shadows } from "../../../style/theme";
+import { IconKcal, IconHeart, IconPeople } from "../../../img/icons/Icons";
+import { useInView } from "react-intersection-observer";
+import { useSpring, animated, interpolate } from "react-spring";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import img1 from '../../../img/images/recipes/recipes1.jpeg'
-import img2 from '../../../img/images/recipes/recipes2.jpeg'
+import img1 from "../../../img/images/recipes/recipes1.jpeg";
+import img2 from "../../../img/images/recipes/recipes2.jpg";
 
-import Image from '../../components/image'
+import Image from "../../components/image";
 
-const SliderNavigationItem = styled(Link)({
-  ...headerSliderNavigation,
-  marginRight: "20px",
-  paddingBottom: "8px",
-  display: "flex",
-  flexWrap: "wrap",
-  justifyContent: "center",
-  whiteSpace: "nowrap" 
-})
-
-const SliderNavigationBar = styled(Div)(({active}) => ({
-  width: "60%",
-  height: "2px",
-  backgroundColor: active ? colors.themeRed2 : "transparent",
-  borderRadius: "4px"
-}))
-
-const imgStyle = {
-  height: "100%", 
-  width: "100%", 
-  objectFit: "cover", 
-  position: "absolute",
-}
-
-const SubItem = ({icon}) => {
-  const IconComp = icon
+const SubItem = ({ icon }) => {
+  const IconComp = icon;
   return (
-    <Div css={{position: "relative", marginRight: "20px"}}>
+    <Div css={{ position: "relative", marginRight: "20px", alignItems: "center" }}>
       <IconComp fill="white" />
-      <span style={{...headerCardSecondary, color: "white", marginLeft: "6px"}}>10</span>
+      <span style={{ ...headerCardSecondary, fontSize: "inherit", color: "white", marginLeft: "6px" }}>10</span>
     </Div>
-  )
-}
+  );
+};
 
 const dataPopularCard = [
   {
@@ -54,116 +30,136 @@ const dataPopularCard = [
     title: "Eggs and salmon",
     chef: "Joan Williams"
   },
-  // {
-  //   img: img2,
-  //   title: "Salad with avocado",
-  //   chef: "Preben Arentoft"
-  // }
-]
+  {
+    img: img2,
+    title: "Salad with avocado",
+    chef: "Preben Arentoft"
+  }
+];
 
-const CardWrapper = animated(Div)
+const CardWrapper = animated(Div);
 
-const CardLayout = styled(Div)(({active}) => ({
-  height: "200px", 
-  width: "85vw", 
+const CardLayout = styled(Div)(({ active }) => ({
+  height: "200px",
+  width: "85vw",
   marginBottom: "25px"
-})) 
+}));
 
-const CardBackgroundDim = styled(Div)(({active}) => ({
+const CardBackgroundDim = styled(Div)(({ active }) => ({
   visibility: active ? "visible" : "hidden",
-  height: "100vh", 
-  width: "100vw", 
-  position: "fixed", 
-  backgroundColor: "white", 
-  top: "0px", 
-  left: "0px", 
-  // zIndex: 1,
+  height: "100vh",
+  width: "100vw",
+  position: "fixed",
+  backgroundColor: "white",
+  top: "0px",
+  left: "0px",
   opacity: active ? 1 : 0,
   transition: "0.3s"
-})) 
+}));
 
-const styleCardContainer = {
+const styleDivAnimated = {
   marginBottom: "25px",
-  overflow: "hidden", 
+  overflow: "hidden",
   flexDirection: "column",
   justifyContent: "flex-end",
   boxShadow: shadows.cardShadowWide
-}
+};
 
-const CardContainer = animated(Div)
+const TextAnimated = animated("h3");
+const DivAnimated = animated(Div);
 
-// const CardContainer = styled(Div)(({active, boundary}) => {
-//   console.log("boundary ",boundary);
- 
+const CARD_PADDING_LEFT = window.innerWidth * ((100 - 85) / 2 / 100);
 
-//   return {
- 
-//   }
-// })
+const styleBottomLeft = {
+  marginLeft: `${CARD_PADDING_LEFT}px`,
+  marginBottom: "15px",
+  position: "absolute",
+  bottom: "0px",
+  transformOrigin: "left center"
+};
 
-// let styleTranslate = {}
-// if (active) {
-//   styleTranslate = {
-//     transform: `translateY(-${boundary.top}px)`
-//   }
-// }
-// width: active ? "100vw" : "85vw",
-// position: active ? "fixed": "absolute",
-// left: active ? 0 : "inherit",
-// borderRadius: active ? 0 : "15px",
-// height: active ? "500px" : "200px",
-// top: active ? boundary.top : 0,
-const TextAnimated = animated("h3")
+const styleBottomRight = {
+  position: "absolute",
+  marginRight: `${CARD_PADDING_LEFT}px`,
+  marginBottom: "15px",
+  bottom: "0",
+  right: "0"
+};
 
-const HomePopularCard = ({img, title, chef}) => {
-  const ref = React.useRef(null)
-  const [active, setState] = useState(false)
-  const [cardBound, setBoundState] = useState({y:0, x:0})
+const styleTopRight = {
+  marginRight: `20px`,
+  marginTop: "20px",
+  position: "absolute",
+  right: 0,
+  top: 0,
+  width: "20px",
+  height: "20px"
+};
 
-  const [springState, setSpringState ] = useSpring(() => ({
+const HomePopularCard = ({ img, title, chef }) => {
+  const ref = React.useRef(null);
+  const [active, setState] = useState(false);
+  const [clientRect, setBoundState] = useState({ card: { y: 0, x: 0 } });
+
+  const [springState, setSpringState] = useSpring(() => ({
     o: 0
-  }))
+  }));
 
-  function handleCardClick () {
+  function handleCardClick() {
     // Locking scrollview
     active
       ? document.getElementById("test").classList.remove("lock")
-      : document.getElementById("test").classList.add("lock")
+      : document.getElementById("test").classList.add("lock");
 
-    
     if (active === false) {
-        const boundingBoxStart = ref.current && ref.current.getBoundingClientRect()
-        setBoundState(boundingBoxStart)
+      const boundingBoxStart = ref.current && ref.current.getBoundingClientRect();
+      console.log("boundingBoxStart ", boundingBoxStart);
+      setBoundState({ card: boundingBoxStart });
     }
 
-    setState(!active)
+    setState(!active);
     setSpringState({
       o: active ? 0 : 1
-    })
+    });
   }
 
-  const HEIGHT_CARD_CLOSED = 200
-  const HEIGHT_CARD_OPEN = 500
-  console.log("ACTIVE ", active);
-  
-  function calcFromTo (value, from, to) {
-    const val = (to - from) * value + from 
-    return val
+  const CHAR_WIDTH = 12;
+  const HEIGHT_CARD_CLOSED = 200;
+  const HEIGHT_CARD_OPEN = 500;
+  const titleLength = title.split(" ").map(i => i.length);
+
+  function calcFromTo(value, from, to) {
+    const val = (to - from) * value + from;
+    return val;
+  }
+
+  function getTranslateAmount(index) {
+    const totalChars = titleLength.reduce((acc, i, arrayIndex) => {
+      if (index > arrayIndex) {
+        return acc + i;
+      }
+      return acc;
+    }, 0);
+    return totalChars;
   }
 
   return (
-    <Div style={{
-      position: "relative", 
-      height: "200px", 
-      width: "85vw", 
-      marginBottom: "25px",
-      // border: "3px solid red",
-      zIndex: 2,
-      }} >
-      {/* <CardBackgroundDim active={active} /> */}
-      <CardContainer ref={ref} onClick={handleCardClick} active={active} 
+    <DivAnimated
+      style={{
+        position: "relative",
+        height: "200px",
+        width: "85vw",
+        marginBottom: "25px",
+        zIndex: springState.o.interpolate(o => (o === 0 ? 0 : 2))
+      }}
+    >
+      <CardBackgroundDim active={active} />
+      <DivAnimated
+        ref={ref}
+        onClick={handleCardClick}
+        active={active}
         style={{
-          ...styleCardContainer,
+          ...styleDivAnimated,
           backgroundImage: `url(${img})`,
           backgroundSize: "100vw",
           backgroundPosition: "center",
@@ -171,47 +167,73 @@ const HomePopularCard = ({img, title, chef}) => {
           height: springState.o.interpolate(o => `${calcFromTo(o, HEIGHT_CARD_CLOSED, HEIGHT_CARD_OPEN)}px`),
           borderRadius: springState.o.interpolate(o => `${calcFromTo(o, 15, 0)}px`),
           position: "absolute",
-          transform: springState.o.interpolate(o => `translate3d(
-            -${calcFromTo(o, 0, cardBound.x)}px, 
-            -${calcFromTo(o, 0, cardBound.y)}px, 
+          transform: springState.o.interpolate(
+            o => `translate3d(
+            -${calcFromTo(o, 0, clientRect.card.x)}px, 
+            ${calcFromTo(o, 0, clientRect.card.y * -1)}px, 
             0px)
-          `),
-        }} 
+          `
+          )
+        }}
       >
-      {
-            title.split(" ").map((mainTitle, index) => (
-              <TextAnimated style={{
-                ...headerCardPrimaryTrendy,
-                // position: "absolute",
-                bottom: "50px",
-                fontSize: springState.o.interpolate(o => `${calcFromTo(o, 1, 2)}rem`),
-                transform: springState.o.interpolate(o => `
-                translate3d(${calcFromTo(o, 0, 7.5)}vw, -${calcFromTo(o, 0, 250)}px, 0)
-                `),
-                }}>{mainTitle}</TextAnimated>
-            ))
-          }
-      </CardContainer>
+        <Div>
+          {title.split(" ").map((mainTitle, index) => {
+            const translateTextYAmount = getTranslateAmount(index) * CHAR_WIDTH;
+            return (
+              <TextAnimated
+                style={{
+                  ...headerCardPrimaryTrendy,
+                  position: "absolute",
+                  bottom: "50px",
+                  marginRight: "5px",
+                  left: "0px",
+                  fontSize: springState.o.interpolate(o => `${calcFromTo(o, 16, 32)}px`),
+                  transform: springState.o.interpolate(
+                    o => `translate3d(
+                    ${translateTextYAmount - translateTextYAmount * o + CARD_PADDING_LEFT}px,
+                    -${calcFromTo(o, 0, 250 - index * 45)}px,
+                    0)
+                  `
+                  )
+                }}
+              >
+                {mainTitle}
+              </TextAnimated>
+            );
+          })}
+          <Div css={styleBottomLeft}>
+            {[IconKcal, IconPeople].map((i, index) => (
+              <SubItem icon={i} key={index} />
+            ))}
+          </Div>
+          <Div css={styleBottomRight}>
+            <span style={{ ...headerCardSecondary, color: "white", marginLeft: "6px" }}>by {chef}</span>
+          </Div>
+          <DivAnimated
+            style={{
+              ...styleTopRight,
+              transform: springState.o.interpolate(o => `translate3d(0, -${calcFromTo(o, 50, 0)}px, 0)`)
+            }}
+          >
+            <IconHeart height="20" width="20" fill="white" />
+          </DivAnimated>
+        </Div>
+      </DivAnimated>
+    </DivAnimated>
+  );
+};
+
+const HomePopular = () => {
+  return (
+    <Div p="4" display="block">
+      <P mb="4" style={headerSliderNavigation}>
+        Populære
+      </P>
+      {dataPopularCard.map((i, index) => (
+        <HomePopularCard key={index} {...i} />
+      ))}
     </Div>
-  )
-}
+  );
+};
 
-   {/* {
-            [IconKcal, IconPeople, IconHeart].map((i,index) =>  <SubItem icon={i} key={index} />)
-          } */}
-          {/* <span style={{...headerCardSecondary, color: "white", marginLeft: "6px"}}>
-            by {chef}
-          </span> */}
-
-  const HomePopular = () => {
-    return (
-      <Div p="4" display="block">
-        <P mb="4" style={headerSliderNavigation}>Populære</P>
-        {
-          dataPopularCard.map((i, index) => <HomePopularCard key={index} {...i} /> )
-        }
-      </Div>
-    )
-  }
-
-export default HomePopular
+export default HomePopular;
